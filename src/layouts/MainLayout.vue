1478e445369cf4ba6f4123ac749a6b2979f84cd5
Chris Fakiolas <fakiolas.ch@gmail.com>
2025-10-20 21:30:21 +0300

    feat: added basic project cards and fetching from the API

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -4,9 +4,8 @@
       <q-toolbar>
         <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
 
-        <q-toolbar-title> Quasar App </q-toolbar-title>
+        <q-toolbar-title> Projects </q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
 
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,13 +1,65 @@
 <template>
   <q-page class="flex flex-center">
-    <img
+    <!-- <img
       alt="Quasar logo"
       src="~assets/quasar-logo-vertical.svg"
       style="width: 200px; height: 200px"
-    />
+    /> -->
+
+    <div class="row">
+      <div class="col-12 projects-container" v-if="totalProjects && totalProjects > 0">
+        <q-card flat bordered class="my-card" v-for="project in projects" :key="project.id">
+          <q-card-section>
+            <div class="text-h6">{{ project.title }}</div>
+          </q-card-section>
+
+          <q-separator inset />
+
+          <q-card-section class="q-pt-none">
+            {{ project.description }}
+          </q-card-section>
+        </q-card>
+      </div>
+      <div class="col-12" v-else>There are no projects yet</div>
+      <div class="col-12">
+        <q-btn v-if="previousPage" color="primary" label="Previous" />
+        <q-btn v-if="nextPage" color="primary" label="Next" />
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script setup>
-//
+import { onMounted, ref } from 'vue'
+import { api } from 'src/boot/axios'
+
+const projects = ref([])
+const totalProjects = ref(null)
+const nextPage = ref(null)
+const previousPage = ref(null)
+
+async function loadProjects() {
+  const res = await api.get('projects/')
+  console.log(res.data.results, res)
+  if (res.status === 200) {
+    projects.value = res.data.results
+    totalProjects.value = res.data.count
+    nextPage.value = res.data.next
+    previousPage.value = res.data.previous
+  }
+}
+
+onMounted(() => {
+  loadProjects()
+})
 </script>
+<style lang="scss" scoped>
+.my-card {
+  width: 100%;
+  max-width: 250px;
+}
+.projects-container {
+  display: flex;
+  gap: 10px;
+}
+</style>
