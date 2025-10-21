64bac6bc339259561324d787e827b3e141cb1cbb
Chris Fakiolas <fakiolas.ch@gmail.com>
2025-10-21 20:46:59 +0300

    feat: removed unused drawer from layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -5,17 +5,8 @@
         <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
 
         <q-toolbar-title> Projects </q-toolbar-title>
-
       </q-toolbar>
     </q-header>
-
-    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
-      <q-list>
-        <q-item-label header> Essential Links </q-item-label>
-
-        <EssentialLink v-for="link in linksList" :key="link.title" v-bind="link" />
-      </q-list>
-    </q-drawer>
 
     <q-page-container>
       <router-view />
