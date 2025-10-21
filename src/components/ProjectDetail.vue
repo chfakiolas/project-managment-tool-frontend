ad1c07bbc8620b95a12968bfc33edf3c5de0d5f0
Chris Fakiolas <fakiolas.ch@gmail.com>
2025-10-21 20:47:55 +0300

    chore: removed unused code from main layout

--- src/components/ProjectDetail.vue
+++ src/components/ProjectDetail.vue
@@ -175,11 +175,11 @@
       <!-- Actions -->
       <q-card-actions align="right" class="q-pa-lg">
         <q-btn flat label="Close" color="grey-7" v-close-popup />
-        <q-btn 
-          unelevated 
-          label="Edit Project" 
-          color="primary" 
-          icon="edit" 
+        <q-btn
+          unelevated
+          label="Edit Project"
+          color="primary"
+          icon="edit"
           @click.stop="editProject"
         />
         <q-btn
